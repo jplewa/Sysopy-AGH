6a115dca2813f207cf5b4630f08c212271eb19d2
Julia Plewa <juliadplewa@gmail.com>
2018-03-17 17:48:19 +0100

    generate and sys copy

--- cw02/zad1/main.c
+++ cw02/zad1/main.c
@@ -1,14 +1,51 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define RECORD_SIZE 100
+void generate (FILE* file, int size, int record_size){
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < record_size - 1; j++){
+            char c = (char)(' ' + rand()%58);
+            fwrite(&c, 1, 1, file);
+        }
+        char c = (char) 10;
+        fwrite(&c, 1, 1, file);
+    }
+}
+void sys_sort (){
 
+}
+void lib_sort(){
 
-void generate (FILE* filename, int n){
-    
+}
+void sys_copy(){
+
+}
+void lib_copy(){
+
 }
 
-int main(int argc, char* argv){
+void parse(int argc, char* argv[]){
+    if (argc < 5) return;
+    if (strncmp(argv[1], "generate", 8) == 0){
+        printf("%s", "ok\n");
+        const char* filename = argv[2];
+        const char* mode = "w";
+        FILE* file =  fopen(filename, mode);
+        char * pEnd;
+        int size = (int) strtol(argv[3],&pEnd,10);
+        int record_size = (int) strtol(argv[4],&pEnd,10);
+        if (size == 0 || record_size == 0) return;
+        else generate(file, size, record_size);
+        fclose(file);
+    }
+}
 
 
-
+int main(int argc, char* argv[]){
+    parse(argc, argv);
 }
