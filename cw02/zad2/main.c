6074749b6c5d524f238882cd4fc0867fc870cd4c
Julia Plewa <juliadplewa@gmail.com>
2018-03-18 17:09:00 +0100

    ex2 both ways

--- cw02/zad2/main.c
+++ cw02/zad2/main.c
@@ -10,8 +10,9 @@
 #include <string.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <ftw.h>
 
-char* permissions(struct stat* path_stat){
+char* permissions(const struct stat* path_stat){
     char* result = malloc(11);
     result = strncpy(result, "----------\0", 11);
     if (S_IRUSR & path_stat -> st_mode) result[1] = 'r';  
@@ -25,14 +26,12 @@ char* permissions(struct stat* path_stat){
     if (S_IXOTH & path_stat -> st_mode) result[9] = 'x'; 
     return result;
 }
-
 bool time_equal(time_t requested, time_t file, int cmp){
     if (cmp == 0 && (int)difftime(file, requested)/86400 == 0) return true;
     if (cmp != 0 && ((difftime(file, requested)*cmp) > 0) && (time_equal(requested, file, 0) == false)) return true;
     else return false; 
 }
-
-void search_dirs(char* current_path, DIR* directory, int cmp, time_t date){
+void search_dirs (char* current_path, DIR* directory, int cmp, time_t date){
     struct dirent* current = readdir(directory);
     if (current == NULL) return;
     struct stat* path_stat = malloc(sizeof(struct stat));
@@ -61,14 +60,55 @@ void search_dirs(char* current_path, DIR* directory, int cmp, time_t date){
             }
         }
     }
-    else printf("Oh dear something went wrong with read()! %s\n", strerror(errno));
     search_dirs(current_path, directory, cmp, date);
 }
-
+void search_dirs_nftw (char* directory, int cmp, time_t date){
+    int flags = 0;
+    flags |= FTW_PHYS;
+    flags |= FTW_MOUNT;
+    int fn (const char* file_path, const struct stat* path_stat, int flag, struct FTW* ftw) {
+        if (S_ISREG(path_stat -> st_mode) && time_equal(date, path_stat -> st_mtime, cmp)){
+            printf("%s\n%10ld\t%s", file_path, (long) path_stat -> st_size, permissions(path_stat));
+            char buff[20]; 
+            struct tm * timeinfo;
+            timeinfo = localtime (&(path_stat -> st_mtime)); 
+            strftime(buff, 20, "%Y-%m-%d %H:%M:%S", timeinfo); 
+            printf("\t%s\n",buff);
+        }
+        return 0;
+    };
+    nftw(directory, fn, 20, flags);
+}
 void parse(int argc, char* argv[]){
     if (argc < 4) return;
     DIR* directory = opendir(argv[1]);
     if (directory == NULL) return;
+    char* full_path = calloc(1024, 1);
+    full_path[0] = '\0';
+    if (argv[1][0] != '/' && argv[1][0] != '.'){
+        char* cwd = getcwd(NULL, 0);
+        strcat(full_path, cwd);
+        strcat(full_path, "/");
+        strcat(full_path, argv[1]);
+    }
+    else if (argv[1][0] == '.' && argv[1][1] == '.'){
+        chdir("..");
+        char* cwd = getcwd(NULL, 0);
+        char* path = malloc(strlen(argv[1]) - 2);
+        for (int i = 0; i < strlen(argv[1])-2; i++) path[i] = argv[1][i+2];    
+        strcat(full_path, cwd);
+        strcat(full_path, path);
+    }
+    else if (argv[1][0] == '.' && argv[1][1] == '/'){
+        char* cwd = getcwd(NULL, 0);
+        char* path = malloc(strlen(argv[1]) - 1);
+        for (int i = 0; i < strlen(argv[1])-2; i++) path[i] = argv[1][i+2];    
+        strcat(full_path, cwd);
+        strcat(full_path, "/");
+        strcat(full_path, path);
+    }
+    else strcat(full_path, argv[1]);
+    if (full_path[strlen(full_path)-1] != '/') strcat(full_path, "/");
     int cmp = ((int) argv[2][0]) - 61;
     if (abs(cmp) > 1){
         printf("ups\n");
@@ -77,9 +117,10 @@ void parse(int argc, char* argv[]){
     struct tm* tm = malloc(sizeof(struct tm));
     if (strptime(argv[3], "%Y-%m-%d", tm) == NULL) return;
     time_t date = mktime(tm);
-    search_dirs(argv[1], directory, cmp, date);
+    search_dirs(full_path, directory, cmp, date);
+    printf("\n");
+    search_dirs_nftw(argv[1], cmp, date);
 }
-
 int main(int argc, char* argv[]){
     parse(argc, argv);
 }
