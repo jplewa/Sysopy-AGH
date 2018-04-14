44a06e5dd8cd1a4aa5c4b89463ae1c63d523bbaa
jplewa <juliadplewa@gmail.com>
2018-04-14 19:50:03 +0200

    final

--- cw04/zad2/main.c
+++ cw04/zad2/main.c
@@ -18,14 +18,6 @@ void exit_handler(int signum, siginfo_t* sig_info, void* sig_context);
 void sigint_handler(int signum, siginfo_t* sig_info, void* sig_context);
 void set_handlers();
 
-const char* msg1 = "Received SIGUSR1 request from process ";
-const char* msg2 = "Sent SIGCONT permission to process ";
-const char* msg3a = "Received SIGRTMIN+";
-const char* msg3b = " signal from ";
-const char* msg4a = "Process ";
-const char* msg4b = " exited with status ";
-const char* msg_sigint = "\nSIGINT signal was received\nAll children have been terminated\n";
-
 int N = 0;
 int K = 0;
 int COUNT = 0;
@@ -146,9 +138,9 @@ void print_number (int n){
 
 void sigusr1_handler(int signum, siginfo_t* sig_info, void* sig_context){
     if (options[1]){
-        write(1, msg1, 38);
+        write(1, "Received SIGUSR1 request from process ", 38);
         print_number((int)sig_info -> si_pid);
-        write(1,"\n\0", 2);
+        write(1, "\n", 1);
     }
     children[K_INDEX] = sig_info -> si_pid;
     K_INDEX++;
@@ -156,29 +148,29 @@ void sigusr1_handler(int signum, siginfo_t* sig_info, void* sig_context){
         for (int i = 0; i < K; i++){
             kill(children[i], SIGCONT);
             if (options[2]){
-                write(1, msg2, 35);
+                write(1, "Sent SIGCONT permission to process ", 35);
                 print_number((int)children[i]);
-                write(1, "\n\0", 2);
+                write(1, "\n", 1);
             }
         }
     }
     else if (K_INDEX > K){
         kill(sig_info -> si_pid, SIGCONT);
         if (options[2]){
-            write(1, msg2, 35);
+            write(1, "Sent SIGCONT permission to process ", 35);
             print_number((int)sig_info -> si_pid);
-            write(1, "\n\0", 2);
+            write(1, "\n", 1);
         }
     }
 }
 
 void real_time_handler(int signum, siginfo_t* sig_info, void* sig_context){
     if (options[3]){
-        write(1, msg3a, 18);
+        write(1, "Received SIGRTMIN+", 18);
         print_number(signum-SIGRTMIN);
-        write(1, msg3b, 13);
+        write(1, " signal from ", 13);
         print_number((int) sig_info -> si_pid);
-        write(1, "\n\0", 2);
+        write(1, "\n", 1);
     }
 }
 
@@ -188,11 +180,11 @@ void exit_handler(int signum, siginfo_t* sig_info, void* sig_context){
     while ((pid = waitpid(-1, &status, WNOHANG)) > 0){
         if (WIFEXITED(status)){
             if (options[4]){
-                write(1, msg4a, 8);
+                write(1, "Process ", 8);
                 print_number(pid);
-                write(1, msg4b, 20);
+                write(1, " exited with status ", 20);
                 print_number(WEXITSTATUS(status));
-                write(1, "\n\0", 2);
+                write(1, "\n", 1);
             }
             COUNT--;
         }
@@ -204,7 +196,7 @@ void sigint_handler(int signum, siginfo_t* sig_info, void* sig_context){
         for (int i = 0; children[i] != 0; i++){
             kill(children[i], SIGKILL);
         }
-        write(1, msg_sigint, 62);
+        write(1, "\nSIGINT signal was received\nAll children have been terminated\n", 62);
         exit(0);
     }
 }
